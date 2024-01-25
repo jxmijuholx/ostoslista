4cd4a1a6d5335e9431ed242fdceb45b3c657614b
jxmijuholx <jami.juhola01@gmail.com>
2024-01-25 21:52:53 +0000

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  // käyttäjän syöte tallennetaan tähän
+  const [item, setItem] = useState('');
+  // ostoslista joka koostuu käyttäjän syötteistä
+  const [items, setItems] = useState([]);
+
+  // Lisää ostoslistaan
+  const addItem = () => {
+      const newItems = [...items, item];
+      setItem('');
+   setItems(newItems);
+  };
+
+  // Tyhjennä ostoslista
+  const clearList = () => {
+   setItems([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.otsikko}>Ostoslista</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Syötä ostos"
+        value={item}
+        onChangeText={(text) => setItem(text)}
+      />
+      <TouchableOpacity style={styles.addNappi} onPress={addItem}>
+        <Text>Lisää</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.clearNappi} onPress={clearList}>
+        <Text>Tyhjennä</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={items}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+      />
     </View>
   );
 }
@@ -13,8 +47,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+  addNappi: {
+    backgroundColor: 'lightgreen',
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  clearNappi: {
+    backgroundColor: 'red',
+    padding: 10,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+  },otsikko:{
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+    color: 'black',
+    fontSize: '50',
+  }
 });
